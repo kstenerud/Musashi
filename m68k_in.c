974774db82479b17c5aefa73d8f6b8ab11640e50
Martin Hellspong <martin.hellspong@factor10.com>
2016-02-04 10:31:37 +0100

    Fix suba operand evaluation order
    The SUBA <ea>, An instruction (both word and long variants) must
    evaluate the source operand before the destination operand (<ea> before
    An), because in case of pi/pd addressing modes, the increment/decrement
    side-effect to AY must happen before reading AX if both AX and AY
    references the same register!

--- m68k_in.c
+++ m68k_in.c
@@ -9450,9 +9450,10 @@ M68KMAKE_OP(suba, 16, ., a)
 
 M68KMAKE_OP(suba, 16, ., .)
 {
+	signed short src = MAKE_INT_16(M68KMAKE_GET_OPER_AY_16);
 	uint* r_dst = &AX;
 
-	*r_dst = MASK_OUT_ABOVE_32(*r_dst - MAKE_INT_16(M68KMAKE_GET_OPER_AY_16));
+	*r_dst = MASK_OUT_ABOVE_32(*r_dst - src);
 }
 
 
@@ -9474,9 +9475,10 @@ M68KMAKE_OP(suba, 32, ., a)
 
 M68KMAKE_OP(suba, 32, ., .)
 {
+	uint src = M68KMAKE_GET_OPER_AY_32;
 	uint* r_dst = &AX;
 
-	*r_dst = MASK_OUT_ABOVE_32(*r_dst - M68KMAKE_GET_OPER_AY_32);
+	*r_dst = MASK_OUT_ABOVE_32(*r_dst - src);
 }
 
 
